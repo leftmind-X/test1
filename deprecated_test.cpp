7e31b81bb3571d0646494cd2708f4f6a39f79694
root <17755335021@139.com>
2020-11-18 21:38:37 +0800

    test 1

---
+++ deprecated_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+[[deprecated("not use this")]]int test(int a , int b);
+
+
+int main()
+
+{
+  std::cout<< "test1"<< std::endl;
+  test(1,3);
+  return 0;  
+}
+
+int test(int a, int b)
+{
+
+cout << a+b << endl;
+
+return 0;
+
+}
